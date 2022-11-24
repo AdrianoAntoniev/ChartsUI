e48e90bf3a93fdc3c9e536bfd32acd95d57d489c
AdrianoAntoniev <adriano.antoniev@outlook.com>
2022-11-23 23:30:45 -0300

    Initial Commit

---
+++ GraphSwiftUI/GraphSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphSwiftUIApp.swift
+//  GraphSwiftUI
+//
+//  Created by Adriano Rodrigues Vieira on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GraphSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
